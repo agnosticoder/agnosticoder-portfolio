17739dbd13f3538463073cfde1b9d2a5681bd787
Satinder Singh <agnosticoder@gmail.com>
2022-04-14 08:33:56 -0400

    Remove sass package and subheading

--- components/App.tsx
+++ components/App.tsx
@@ -2,7 +2,6 @@ const App = () => {
     return (
         <div className=''>
             <h1 className='text-slate-800 font-bold'>Agnosticoder Porfolio</h1>
-            <h2 className=''>Content color has bee applied using css modules but using scss</h2>
         </div>
     );
 };
