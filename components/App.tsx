181924627ec497c487b125831d03f6f54004d606
Satinder Singh <agnosticoder@gmail.com>
2022-04-14 08:49:00 -0400

    First preview commit

--- components/App.tsx
+++ components/App.tsx
@@ -2,6 +2,7 @@ const App = () => {
     return (
         <div className=''>
             <h1 className='text-slate-800 font-bold'>Agnosticoder Porfolio</h1>
+            <h2>Preview branch deloyment changes</h2>
         </div>
     );
 };
