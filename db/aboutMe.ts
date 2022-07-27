283a3a4aa7eef2efa410b5b2cd0cae4d2d1353da
Satinder Singh <agnosticoder@gmail.com>
2022-07-26 20:58:36 -0400

    edit about me

--- db/aboutMe.ts
+++ db/aboutMe.ts
@@ -1,5 +1,5 @@
 export const aboutMe = `
-Hey, I am Satinder Singh, a 28 years old software developer based in Toronto, Canada. My passion is to make software for the web. I am self-taught. Before that, I did a Bachelor's in Mechanical Engineering and a Postgrad in Supply Chain Management. My journey to coding has been a roller coaster. Please read my full story [here](/mystory)
+Hey, I am Satinder Singh, a 28 years old software developer based in Toronto, Canada. My passion is to make software for the web. I am self-taught. Before that, I did a Bachelor's in Mechanical Engineering and a Postgrad in Supply Chain Management. My journey to coding has been a roller coaster. You can read my full story [here](/mystory)
 
 ### Education
 
