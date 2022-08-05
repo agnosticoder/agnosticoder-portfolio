335a3c6ad859f21639a5f54a1e9e672a3c05b48e
Satinder Singh <agnosticoder@gmail.com>
2022-08-05 04:08:21 -0400

    Add progress loading bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,33 @@ import '../styles/main.css'
 import 'highlight.js/styles/atom-one-dark.css';
 import type { AppProps } from 'next/app'
 import Layout from '../layouts/Layout';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import NProgress from 'nprogress';
 
 function MyApp({ Component, pageProps }: AppProps) {
+    const router = useRouter()
+
+    useEffect(() => {
+      const handleStart = (url: any) => {
+        console.log(`Loading: ${url}`)
+        NProgress.start()
+      }
+      const handleStop = () => {
+        NProgress.done()
+      }
+  
+      router.events.on('routeChangeStart', handleStart)
+      router.events.on('routeChangeComplete', handleStop)
+      router.events.on('routeChangeError', handleStop)
+  
+      return () => {
+        router.events.off('routeChangeStart', handleStart)
+        router.events.off('routeChangeComplete', handleStop)
+        router.events.off('routeChangeError', handleStop)
+      }
+    }, [router])
+
     return (
         <>
             <Layout>
